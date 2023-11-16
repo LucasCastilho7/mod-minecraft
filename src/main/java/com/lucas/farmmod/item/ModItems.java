3e6601177cc770e6941656a79b1b3e14670728db
GabrieleFasolo <gabrielefasolo23@gmail.com>
2023-11-15 21:53:40 -0300

    adding platinum ore

--- src/main/java/com/lucas/farmmod/item/ModItems.java
+++ src/main/java/com/lucas/farmmod/item/ModItems.java
@@ -13,11 +13,18 @@ public class ModItems {
     public static final DeferredRegister<Item> ITEMS =
             DeferredRegister.create(ForgeRegistries.ITEMS, FarmMod.MOD_ID);
 
+    public static final RegistryObject<Item> PLATINUM_INGOT = ITEMS.register("platinum_ingot", () -> new Item(new Item.Properties()));
+
+    public static final RegistryObject<Item> RAW_PLATINUM_ORE = ITEMS.register("raw_platinum_ore", () -> new Item(new Item.Properties()));
+
+    public static final RegistryObject<Item> PLATINUM_NUGGET = ITEMS.register("platinum_nugget", () -> new Item(new Item.Properties()));
+
     public static final RegistryObject<Item> DILDO = ITEMS.register("dildo",
             () -> new Item(new Item.Properties()));
     public static final RegistryObject<Item> CONDOM = ITEMS.register("condom",
             () -> new Item(new Item.Properties()));
 
-
-    public static void register(IEventBus eventBus) { ITEMS.register(eventBus); }
+    public static void register(IEventBus eventBus) {
+        ITEMS.register(eventBus);
+    }
 }
