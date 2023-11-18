7529e56540f81e9d12580101488a84df207a4f04
GabrieleFasolo <gabrielefasolo23@gmail.com>
2023-11-18 02:02:05 -0300

    migrating to 1.20.1

--- src/main/java/com/lucas/farmmod/item/ModItems.java
+++ src/main/java/com/lucas/farmmod/item/ModItems.java
@@ -1,8 +1,6 @@
 package com.lucas.farmmod.item;
 
 import com.lucas.farmmod.FarmMod;
-import com.lucas.farmmod.tab.ModCreativeModeTab;
-import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.Item;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
